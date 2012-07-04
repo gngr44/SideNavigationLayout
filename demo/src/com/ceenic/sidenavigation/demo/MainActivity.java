7aefaa0f4a53dffa749dd5834095e56ab6a9b591
Chu Ngo <gngr44@gmail.com>
2012-07-03 22:55:17 -0700

    Initial commit

---
+++ demo/src/com/ceenic/sidenavigation/demo/MainActivity.java
@@ -0,0 +1,138 @@
+package com.ceenic.sidenavigation.demo;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.AbsListView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.ceenic.sidenavigation.R;
+import com.ceenic.sidenavigation.SideNavigationLayout;
+import com.ceenic.sidenavigation.SideNavigationLayout.SideNavigationListener;
+
+public class MainActivity extends Activity implements SideNavigationListener {
+
+    private SideNavigationLayout mSideNavigationView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mSideNavigationView = (SideNavigationLayout) findViewById(R.id.side_nav);
+        mSideNavigationView.setNavigationListener(this);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(new TestAdapter());
+        ListView listView2 = (ListView) findViewById(R.id.list2);
+        listView2.setAdapter(new TestAdapter2());
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_toggle_nav) {
+            if (mSideNavigationView.isShowingNavigationView()) {
+                mSideNavigationView.showContentView();
+            } else {
+                mSideNavigationView.showNavigationView();
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class TestAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 100;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            // TODO Auto-generated method stub
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = new TextView(parent.getContext());
+                AbsListView.LayoutParams params = new AbsListView.LayoutParams(
+                        LayoutParams.MATCH_PARENT, 100);
+                convertView.setLayoutParams(params);
+            }
+            TextView textView = (TextView) convertView;
+            textView.setText("TExt");
+            return convertView;
+        }
+
+    }
+
+    private class TestAdapter2 extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 100;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            // TODO Auto-generated method stub
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = new TextView(parent.getContext());
+                AbsListView.LayoutParams params = new AbsListView.LayoutParams(
+                        LayoutParams.MATCH_PARENT, 400);
+                convertView.setLayoutParams(params);
+            }
+            TextView textView = (TextView) convertView;
+            textView.setBackgroundColor(Color.BLACK);
+            textView.setTextColor(Color.WHITE);
+            textView.setText("This is your content");
+            return convertView;
+        }
+
+    }
+
+    @Override
+    public void onShowNavigationView(SideNavigationLayout view) {
+        Log.d("TEST", "navigation");
+        // getActionBar().setTitle("Navigation");
+    }
+
+    @Override
+    public void onShowContentView(SideNavigationLayout view) {
+        Log.d("TEST", "content");
+        // getActionBar().setTitle("Content");
+
+    }
+}
---
+++ src/com/ceenic/sidenavigation/SideNavigationLayout.java
@@ -0,0 +1,246 @@
+package com.ceenic.sidenavigation;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.widget.FrameLayout;
+import android.widget.Scroller;
+
+/**
+ * A custom FrameLayout that copies the side navigation UI design pattern found
+ * in the YouTube app. It expects two child views to be added to it. The first
+ * one will be the navigation view and the second one will be the content view.
+ */
+public class SideNavigationLayout extends FrameLayout {
+
+    public interface SideNavigationListener {
+        public void onShowNavigationView(SideNavigationLayout view);
+
+        public void onShowContentView(SideNavigationLayout view);
+    }
+
+    private static final int SCROLL_DURATION = 500;
+
+    private Scroller mScroller;
+
+    private VelocityTracker mTracker;
+
+    private SideNavigationListener mListener;
+
+    private float mCurrX;
+
+    private float mCurrY;
+
+    private int mOffsetX;
+
+    private int mTouchSlop;
+
+    private int mMinimumVelocity;
+
+    private int mMaximumVelocity;
+
+    private boolean mScrolledHorizontally;
+
+    private boolean mScrolledVertically;
+
+    private boolean mShowingNavigation;
+
+    public SideNavigationLayout(Context context) {
+        super(context);
+        init();
+    }
+
+    public SideNavigationLayout(Context context, AttributeSet attrs,
+            int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    public SideNavigationLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init() {
+        mScroller = new Scroller(getContext());
+        ViewConfiguration viewConfiguration = ViewConfiguration
+                .get(getContext());
+        mTouchSlop = viewConfiguration.getScaledTouchSlop();
+        mMinimumVelocity = viewConfiguration.getScaledMinimumFlingVelocity();
+        mMaximumVelocity = viewConfiguration.getScaledMaximumFlingVelocity();
+    }
+
+    public void setNavigationListener(SideNavigationListener listener) {
+        mListener = listener;
+    }
+
+    public void showNavigationView() {
+        mScroller.startScroll((int) mOffsetX, 0,
+                (int) (getNavigationViewWidth() - mOffsetX), SCROLL_DURATION);
+        requestLayout();
+        invalidate();
+    }
+
+    public void showContentView() {
+        mScroller.startScroll((int) mOffsetX, 0, (int) -mOffsetX, 0,
+                SCROLL_DURATION);
+        requestLayout();
+        invalidate();
+    }
+
+    public boolean isShowingNavigationView() {
+        return mShowingNavigation;
+    }
+
+    @Override
+    public void computeScroll() {
+        super.computeScroll();
+        if (mScroller.isFinished()) {
+            return;
+        }
+
+        mScroller.computeScrollOffset();
+        mOffsetX = mScroller.getCurrX();
+        requestLayout();
+        invalidate();
+
+        if (mScroller.isFinished()) {
+            boolean wasShowingNavigation = mShowingNavigation;
+            mShowingNavigation = mOffsetX != 0;
+            getChildAt(0).setVisibility(
+                    mShowingNavigation ? View.VISIBLE : View.GONE);
+            if (wasShowingNavigation != mShowingNavigation) {
+                if (mListener != null) {
+                    if (mShowingNavigation) {
+                        mListener.onShowNavigationView(this);
+                    } else {
+                        mListener.onShowContentView(this);
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        boolean shouldIntercept = false;
+        final int action = event.getAction();
+        if (action == MotionEvent.ACTION_DOWN) {
+            mScroller.forceFinished(true);
+            mTracker = VelocityTracker.obtain();
+            mTracker.addMovement(event);
+            mCurrX = event.getX();
+            mCurrY = event.getY();
+            mScrolledHorizontally = false;
+            mScrolledVertically = false;
+            if (mShowingNavigation && event.getX() > getNavigationViewWidth()) {
+                shouldIntercept = true;
+            }
+        } else if (action == MotionEvent.ACTION_MOVE) {
+            if (mScrolledVertically) {
+                // Don't intercept if the user has already scrolled vertically.
+            } else if (Math.abs(mCurrY - event.getY()) > mTouchSlop) {
+                mScrolledVertically = true;
+            } else if (Math.abs(mCurrX - event.getX()) > mTouchSlop) {
+                mScrolledHorizontally = true;
+                shouldIntercept = true;
+                getChildAt(0).setVisibility(View.VISIBLE);
+            }
+        } else if (action == MotionEvent.ACTION_UP
+                || action == MotionEvent.ACTION_CANCEL) {
+            mTracker.recycle();
+            mTracker = null;
+            scrollToNavigationOrContentView();
+        }
+        return shouldIntercept;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int action = event.getAction();
+        if (action == MotionEvent.ACTION_DOWN) {
+            return true;
+        } else if (action == MotionEvent.ACTION_MOVE) {
+            mScrolledHorizontally = true;
+            mTracker.addMovement(event);
+
+            final float x = event.getX();
+            final float diffX = x - mCurrX;
+            final int oldOffset = mOffsetX;
+
+            mOffsetX += diffX;
+            mOffsetX = Math.max(0, mOffsetX);
+            mOffsetX = Math.min(getNavigationViewWidth(), mOffsetX);
+            mCurrX = x;
+            if (oldOffset != mOffsetX) {
+                requestLayout();
+            }
+            return true;
+        } else if (action == MotionEvent.ACTION_UP
+                || action == MotionEvent.ACTION_CANCEL) {
+            mTracker.addMovement(event);
+            // We intercepted the event because the user either
+            // 1) scrolled horizontally OR
+            // 2) tapped on the content view while the navigation view is
+            // showing
+            if (mScrolledHorizontally) {
+                // If the user flinged, we should scroll in the direction of the
+                // fling. Otherwise, we scroll whichever one we're closer to.
+                mTracker.computeCurrentVelocity(1000, mMaximumVelocity);
+                float velocity = mTracker.getXVelocity();
+                if (Math.abs(velocity) > mMinimumVelocity) {
+                    if (velocity > 0) {
+                        showNavigationView();
+                    } else {
+                        showContentView();
+                    }
+                } else {
+                    scrollToNavigationOrContentView();
+                }
+            } else {
+                // We want to show the content view if the user tapped on it
+                // while the navigation is showing.
+                showContentView();
+            }
+            mTracker.recycle();
+            mTracker = null;
+        }
+        return super.onTouchEvent(event);
+    }
+
+    /**
+     * Scroll to the navigation of content view depending on whichever one the
+     * offset is closer to.
+     */
+    private void scrollToNavigationOrContentView() {
+        if (mOffsetX < (getNavigationViewWidth() / 2)) {
+            showContentView();
+        } else {
+            showNavigationView();
+        }
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right,
+            int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        if (getChildCount() > 1) {
+            View contentView = getChildAt(1);
+            int childLeft = contentView.getLeft() + mOffsetX;
+            int childTop = contentView.getTop();
+            int childRight = contentView.getRight() + mOffsetX;
+            int childBottom = contentView.getBottom();
+            contentView.layout(childLeft, childTop, childRight, childBottom);
+        }
+    }
+
+    private int getNavigationViewWidth() {
+        if (getChildCount() > 0) {
+            return getChildAt(0).getWidth();
+        }
+        return 0;
+    }
+}
